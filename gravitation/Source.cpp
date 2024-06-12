a509e006ee48540fd653ae8e00cc2da984dc1d4b
Curtis with a T <curtisahlin@gmail.com>
2024-06-12 18:08:36 +0100

    Cleaned up project

--- gravitation/Source.cpp
+++ gravitation/Source.cpp
@@ -2,35 +2,52 @@
 	a gravity simulation where tonnes of particles circle the mouse position
 */
 
+// TODO improvements
+// [] - use custom random
+// [] - OOP
+// [] - Youtube video with 3 million particles and 4 black holes
+
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <thread>
 
 #include <ctime>
-#include <array>
 #include <future>
 #include <string>
 
 #include <sstream>
 #include "process.h"
 
-const unsigned int blackHoleCount = 4;
-const unsigned int particleCount = 600'000;
-const unsigned int batchSizes = particleCount/8;
-
-const unsigned int defcol = 10;
-
+// SFML settings
 constexpr unsigned int screen_width = 1920;
 constexpr unsigned int screen_height = 1080;
 
-const sf::Vector2f scale(0.01, 0.01);
+const sf::Vector2f simulation_scale = { 0.01f, 0.01f };
 
-const unsigned int borderWidth = screen_width / scale.x;
-const unsigned int borderHeight = screen_height / scale.y;
+const auto border_width  = static_cast<float>(screen_width / simulation_scale.x);
+const auto border_height = static_cast<float>(screen_height / simulation_scale.y);
+const sf::FloatRect bounds = { 0, 0, border_width, border_height };
+
+
+// Simulation Settings
+constexpr unsigned number_of_black_holes = 3u;
+constexpr unsigned number_of_stars = 600'000u;
+
+constexpr float cosmic_speed_limit = 190000.f;
+
+
+// Multi-threading settings
+constexpr unsigned threads = 8u;
+constexpr unsigned threading_batches = number_of_stars / threads;
+
+
+// Graphical Settings
+constexpr unsigned star_color = 30;
+
+
 
 float fa1 = 200000;
 constexpr unsigned int fa2 = 20;
-float cosmicSpeedLimit = 190000;
 
 bool paused = false;
 bool draw = true;
@@ -44,12 +61,12 @@ float randfloat(float start, float end) {
 	return (float(rand()) / float((RAND_MAX)) * (end - start)) + start;
 }
 
-void updatePoint(sf::Vertex &point, sf::Vector2f &velocity, sf::VertexArray* blackHoles) {
-	gravitate(fa1, blackHoles, point, velocity, borderWidth, borderHeight);
-	speed_limit(cosmicSpeedLimit, velocity);
-	border(borderWidth, borderHeight, point.position, velocity);
+void updatePoint(sf::Vertex &point, sf::Vector2f &velocity, sf::VertexArray* blackHoles)
+{
+	gravitate(fa1, blackHoles, point, velocity, border_width, border_height);
+	speed_limit(cosmic_speed_limit, velocity);
+	border(bounds, point.position);
 
-	// updating the star's position
 	point.position += velocity;
 }
 
@@ -74,13 +91,14 @@ void updateBatchOfPoints(int startI, int endI, sf::VertexArray* stars,
 
 void run_sim(sf::RenderWindow& window,
 	sf::VertexArray* stars, std::vector<sf::Vector2f>* star_velocities,
-	sf::VertexArray* blackHoles, std::vector<sf::Vector2f>* blackHole_velocities) {
+	sf::VertexArray* blackHoles, std::vector<sf::Vector2f>* blackHole_velocities)
+{
 
 	sf::Clock clock;
 
 	sf::RenderStates states;
 	sf::Transform transform;
-	transform.scale(scale);
+	transform.scale(simulation_scale);
 
 	states.transform = transform;
 	states.blendMode = sf::BlendAdd;
@@ -91,39 +109,34 @@ void run_sim(sf::RenderWindow& window,
 
 	sf::Clock fps_display_clock = sf::Clock::Clock();
 	
-	while (window.isOpen()) {
+	while (window.isOpen()) 
+	{
 
 		// event handeler
 		sf::Event event;
-		while (window.pollEvent(event)) {
+		while (window.pollEvent(event)) 
+		{
 			if (event.type == sf::Event::Closed)
 				window.close();
 
-			else if (event.type == sf::Event::KeyPressed) {
+			else if (event.type == sf::Event::KeyPressed) 
+			{
 				if (event.key.code == sf::Keyboard::Space)
 					paused = not paused;
 
 				else if (event.key.code == sf::Keyboard::D)
 					draw = not draw;
 
-				else if (event.key.code == sf::Keyboard::Q) {
+				else if (event.key.code == sf::Keyboard::Q) 
+				{
 					fa1 += 1;
 					std::cout << fa1 << "\n";
 				}
 
-				else if (event.key.code == sf::Keyboard::W) {
+				else if (event.key.code == sf::Keyboard::W) 
+				{
 					fa1 -= 1;
 					std::cout << fa1 << "\n";
-				}
-
-				else if (event.key.code == sf::Keyboard::E) {
-					cosmicSpeedLimit += 4;
-					std::cout << cosmicSpeedLimit << "\n";
-				}
-
-				else if (event.key.code == sf::Keyboard::R) {
-					cosmicSpeedLimit -= 4;
-					std::cout << cosmicSpeedLimit << "\n";
 				}
 			}
 		}
@@ -135,24 +148,27 @@ void run_sim(sf::RenderWindow& window,
 		// updating particles
 		int prev_i = 0;
 		std::vector<std::thread> threads;
-		threads.reserve((int)(particleCount / batchSizes));
+		threads.reserve((int)(number_of_stars / threading_batches));
 
-		for (size_t i = batchSizes; i <= particleCount; i += batchSizes) {
-			threads.emplace_back(std::thread(updateBatchOfPoints, prev_i, i, stars, star_velocities, blackHoles));
+		for (size_t i = threading_batches; i <= number_of_stars; i += threading_batches) 
+		{
+			threads.emplace_back(updateBatchOfPoints, prev_i, i, stars, star_velocities, blackHoles);
 			prev_i = i;
 		}
 
-		for (auto& th : threads) {
+		for (auto& th : threads) 
+		{
 			th.join();
 		}
 
 	
 
-		for (size_t i = 0; i < blackHoleCount; i++) {
-			BHgravitate(fa2, blackHoles, (*blackHoles)[i].position, (*blackHole_velocities)[i], borderWidth, borderHeight);
+		for (size_t i = 0; i < number_of_black_holes; i++) 
+		{
+			BHgravitate(fa2, blackHoles, (*blackHoles)[i].position, (*blackHole_velocities)[i], border_width, border_height);
 			speed_limit(3, (*blackHole_velocities)[i]);
 				
-			border(borderWidth, borderHeight, (*blackHoles)[i].position, (*blackHole_velocities)[i]);
+			border(bounds, (*blackHoles)[i].position);
 			//(*blackHole_velocities)[i] += sf::Vector2f(randfloat(-0.001f, 0.001f), randfloat(-0.001f, 0.001f));
 			(*blackHoles)[i].position += (*blackHole_velocities)[i];		
 		}
@@ -176,26 +192,27 @@ void run_sim(sf::RenderWindow& window,
 	}
 }
 
-void initilise(sf::RenderWindow& window) {
+void initilise(sf::RenderWindow& window)
+{
 	// creating the stars array
-	sf::VertexArray stars = sf::VertexArray(sf::Points, particleCount);
-	std::vector<sf::Vector2f> star_velocities(particleCount);
+	sf::VertexArray stars = sf::VertexArray(sf::Points, number_of_stars);
+	std::vector<sf::Vector2f> star_velocities(number_of_stars);
 
 	// creating the blackHoles array
-	sf::VertexArray blackHoles(sf::Points, blackHoleCount);
-	std::vector<sf::Vector2f> blackHole_velocities(blackHoleCount);
+	sf::VertexArray blackHoles(sf::Points, number_of_black_holes);
+	std::vector<sf::Vector2f> blackHole_velocities(number_of_black_holes);
 
 	// initilising the stars
 	for (size_t i = 0; i < star_velocities.size(); i++) {
-		stars[i].position = sf::Vector2f(randfloat(0, borderWidth), randfloat(0, borderHeight));
-		stars[i].color = sf::Color(defcol, defcol, defcol);
+		stars[i].position = sf::Vector2f(randfloat(0, border_width), randfloat(0, border_height));
+		stars[i].color = sf::Color(star_color, star_color, star_color);
 		star_velocities[i] = sf::Vector2f(randfloat(-1, 1), randfloat(-1, 1));
 	}
 
 
 	// initilising the black holes
 	for (size_t i = 0; i < blackHole_velocities.size(); i++) {
-		blackHoles[i].position = sf::Vector2f(randfloat(0, borderWidth), randfloat(0, borderHeight));
+		blackHoles[i].position = sf::Vector2f(randfloat(0, border_width), randfloat(0, border_height));
 		blackHoles[i].color = sf::Color(255, 20, 255);
 		blackHole_velocities[i] = sf::Vector2f(randfloat(-0.1, 0.1), randfloat(-0.1, 0.1));
 	}
@@ -209,7 +226,7 @@ void initilise(sf::RenderWindow& window) {
 
 int main() {
 	// initilising random
-	std::srand(static_cast<unsigned>(time(NULL)));
+	std::srand(static_cast<unsigned>(time(nullptr)));
 
 	// initilising screen variables
 	sf::RenderWindow window(sf::VideoMode(screen_width, screen_height), "Galaxy simulation");
--- gravitation/process.h
+++ gravitation/process.h
@@ -1,60 +1,64 @@
 #pragma once
 
-#include <algorithm>
-#include <execution>
-#include <numeric>
-
 #include <iostream>
 
 
-double wrappedDistanceSquared(sf::Vector2f position1, sf::Vector2f position2, int screenWidth, int screenHeight) {
+inline double wrapped_distance_squared(const sf::Vector2f position1, const sf::Vector2f position2, const unsigned screen_width, const unsigned screen_height)
+{
 	float dx = abs(position2.x - position1.x);
-	float  dy = abs(position2.y - position1.y);
+	float dy = abs(position2.y - position1.y);
 
-	if (dx > screenWidth / 2)
-		dx = screenWidth - dx;
-	if (dy > screenHeight / 2)
-		dy = screenHeight - dy;
+	if (dx > screen_width / 2)
+		dx = screen_width - dx;
+	if (dy > screen_height / 2)
+		dy = screen_height - dy;
 
 	return dx*dx + dy*dy;
 }
 
-sf::Vector2f directionCalculator(sf::Vector2f position1, sf::Vector2f position2, int screenWidth, int screenHeight) {
-	float StartX = position1.x; float StartY = position1.y;
-	float EndX = position2.x; float EndY = position2.y;
+inline sf::Vector2f direction_calculator(const sf::Vector2f position1, const sf::Vector2f position2, const unsigned screen_width, const unsigned screen_height)
+{
+	const float start_x = position1.x;
+	const float start_y = position1.y;
+	const float end_x = position2.x;
+	const float end_y = position2.y;
 
-	float dist_x = abs(StartX - EndX);
+	const float dist_x = abs(start_x - end_x);
+	const float dist_y = abs(start_y - end_y);
+
 	float result_x = 0.0f;
-
-	if (dist_x < screenWidth / 2)
+	if (dist_x < screen_width / 2)
 		result_x = dist_x;
 	else
-		result_x = dist_x - screenWidth;
+		result_x = dist_x - screen_width;
 
-	if (StartX > EndX)
+	if (start_x > end_x)
 		result_x *= -1;
 
 
-	float dist_y = abs(StartY - EndY);
-	float result_y = 0;
-
-	if (dist_y < screenHeight / 2)
+	
+	float result_y = 0.f;
+	if (dist_y < screen_height / 2)
 		result_y = dist_y;
 	else
-		result_y = dist_y - screenHeight;
+		result_y = dist_y - screen_height;
 
-	if (StartY > EndY)
+	if (start_y > end_y)
 		result_y *= -1;
 
-	return sf::Vector2f(result_x, result_y);
+	return {result_x, result_y};
 }
 
 
-sf::Vector2f gravitate(float fa, sf::VertexArray* blackHoles, sf::Vertex &point, sf::Vector2f &velocity,
-	int screenWidth, int screenHeight) {
-	for (int i = 0; i < (*blackHoles).getVertexCount(); i++) {
-		float force = (fa / wrappedDistanceSquared(point.position, (*blackHoles)[i].position, screenWidth, screenHeight)) * 0.01;
-		sf::Vector2f direction = directionCalculator(point.position, (*blackHoles)[i].position, screenWidth, screenHeight);
+inline sf::Vector2f gravitate(const float gravitational_const, sf::VertexArray* black_holes, const sf::Vertex &point, sf::Vector2f &velocity, const unsigned screen_width, const unsigned screen_height)
+{
+	const size_t black_hole_count = black_holes->getVertexCount();
+	for (size_t i = 0; i < black_hole_count; i++) 
+	{
+		const sf::Vector2f black_hole_center = (*black_holes)[i].position;
+		const float wrapped_dist_sq = wrapped_distance_squared(point.position, black_hole_center, screen_width, screen_height);
+		const float force = gravitational_const * (1 / wrapped_dist_sq);
+		sf::Vector2f direction = direction_calculator(point.position, black_hole_center, screen_width, screen_height);
 		velocity += (direction * force);
 	}
 	return velocity;
@@ -66,9 +70,9 @@ sf::Vector2f BHgravitate(float fa, sf::VertexArray* blackHoles, sf::Vector2f &po
 
 	for (size_t i = 0; i < (*blackHoles).getVertexCount(); i++) {
 		if ((*blackHoles)[i].position != position) {
-			float d = wrappedDistanceSquared(position, (*blackHoles)[i].position, screenWidth, screenHeight);
+			float d = wrapped_distance_squared(position, (*blackHoles)[i].position, screenWidth, screenHeight);
 			float force = fa / d;
-			sf::Vector2f direction = directionCalculator(position, (*blackHoles)[i].position, screenWidth, screenHeight);
+			sf::Vector2f direction = direction_calculator(position, (*blackHoles)[i].position, screenWidth, screenHeight);
 
 			velocity += direction * force;
 		}
@@ -77,34 +81,30 @@ sf::Vector2f BHgravitate(float fa, sf::VertexArray* blackHoles, sf::Vector2f &po
 }
 
 
-sf::Vector2f speed_limit(int max_speed, sf::Vector2f &velocity) {
-	if (velocity.x > max_speed)
-		velocity.x = max_speed;
+inline void speed_limit(const float max_speed, sf::Vector2f &velocity)
+{
+	const float speed_sq = velocity.x * velocity.x + velocity.y * velocity.y;
 
-	else if (velocity.x < -max_speed)
-		velocity.x = -max_speed;
-
-	if (velocity.y < -max_speed)
-		velocity.y = -max_speed;
-	else if (velocity.y > max_speed)
-		velocity.y = max_speed;
-	
-	return velocity;
+	if (speed_sq > max_speed * max_speed)
+	{
+		const float speed = sqrt(speed_sq);
+		const sf::Vector2f norm_vel = velocity / speed;
+		velocity = norm_vel * max_speed;
+	}
 }
 
 
-sf::Vector2f border(int borderX, int borderY, sf::Vector2f &position, sf::Vector2f &velocity) {
-	if (position.x > borderX)
-		position.x = 0;
+inline void border(const sf::Rect<float>& bounds, sf::Vector2f &position)
+{
+	if (position.x > bounds.left + bounds.width)
+		position.x -= bounds.left + bounds.width;
 	
-	else if (position.x < 0)
-		position.x = borderX;
+	else if (position.x < bounds.left)
+		position.x += bounds.left + bounds.height;
 
-	if (position.y < 0)
-		position.y = borderY;
+	if (position.y < bounds.top)
+		position.y += bounds.top + bounds.height;
 	
-	else if (position.y > borderY)
-		position.y = 0;
-	
-	return position;
+	else if (position.y > bounds.top + bounds.height)
+		position.y -= bounds.top + bounds.height;
 }
